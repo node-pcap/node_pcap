c4368962390c629e4a67ffaa60e8299b1ddb9b2c
Liam Mitchell <liamkarlmitchell@gmail.com>
2014-05-08 01:01:16 +1200

    Trying to find assert problem,
    Also fixed a bug? where program would just exit when trying to write
    NULL value as String::New(NULL);

--- pcap_binding.cc
+++ pcap_binding.cc
@@ -25,11 +25,37 @@ const char* inet_ntop(int af, const void* src, char* dst, int cnt){
     srcaddr.sin_family = af;
     if (WSAAddressToString((struct sockaddr*) &srcaddr, sizeof(struct sockaddr_in), 0, dst, (LPDWORD) &cnt) != 0) {
         DWORD rv = WSAGetLastError();
-        printf("WSAAddressToString() : %d\n",rv);
         return NULL;
     }
     return dst;
 }
+
+/*const char *inet_ntop(int af, const void *src, char *dst, socklen_t cnt)
+{
+    if (af == AF_INET)
+    {
+        struct sockaddr_in in;
+        memset(&in, 0, sizeof(in));
+        in.sin_family = AF_INET;
+        memcpy(&in.sin_addr, src, sizeof(struct in_addr));
+        getnameinfo((struct sockaddr *)&in, sizeof(struct
+            sockaddr_in), dst, cnt, NULL, 0, NI_NUMERICHOST);
+		printf("DST: %s\n",dst);
+        return dst;
+    }
+    else if (af == AF_INET6)
+    {
+        struct sockaddr_in6 in;
+        memset(&in, 0, sizeof(in));
+        in.sin6_family = AF_INET6;
+        memcpy(&in.sin6_addr, src, sizeof(struct in_addr6));
+        getnameinfo((struct sockaddr *)&in, sizeof(struct
+            sockaddr_in6), dst, cnt, NULL, 0, NI_NUMERICHOST);
+		printf("DST6: %s\n",dst);
+        return dst;
+    }
+    return NULL;
+}*/
 
 #elif _linux_
 #include <sys/time.h>
@@ -49,17 +75,23 @@ void SetAddrStringHelper(const char* key, sockaddr *addr, Local<Object> Address)
     char dst_addr[INET6_ADDRSTRLEN + 1] = {0};
     char* src = 0;
     socklen_t size = 0;
+	
     if(addr->sa_family == AF_INET){
       struct sockaddr_in* saddr = (struct sockaddr_in*) addr;
       src = (char*) &(saddr->sin_addr);
       size = INET_ADDRSTRLEN;
-    }else{
+    } else {
       struct sockaddr_in6* saddr6 = (struct sockaddr_in6*) addr;
       src = (char*) &(saddr6->sin6_addr);
       size = INET6_ADDRSTRLEN;
     }
+	
     const char* address = inet_ntop(addr->sa_family, src, dst_addr, size);
-    Address->Set(String::New(key), String::New(address));
+	if (address) {
+		Address->Set(String::New(key), String::New(address));
+	} else {
+		Address->Set(String::New(key), String::New(""));
+	}
   }
 }
 
@@ -73,13 +105,12 @@ FindAllDevs(const Arguments& args)
     if (pcap_findalldevs(&alldevs, errbuf) == -1 || alldevs == NULL) {
         return ThrowException(Exception::TypeError(String::New(errbuf)));
     }
-
+	
     Local<Array> DevsArray = Array::New();
 
     int i = 0;
     for (cur_dev = alldevs ; cur_dev != NULL ; cur_dev = cur_dev->next, i++) {
         Local<Object> Dev = Object::New();
-
         Dev->Set(String::New("name"), String::New(cur_dev->name));
         if (cur_dev->description != NULL) {
             Dev->Set(String::New("description"), String::New(cur_dev->description));
@@ -87,16 +118,16 @@ FindAllDevs(const Arguments& args)
         Local<Array> AddrArray = Array::New();
         int j = 0;
         for (pcap_addr_t *cur_addr = cur_dev->addresses ; cur_addr != NULL ; cur_addr = cur_addr->next, j++) {
-	  if (cur_addr->addr){
-		int af = cur_addr->addr->sa_family;
-		if(af == AF_INET || af == AF_INET6){
-		  Local<Object> Address = Object::New();
-		  SetAddrStringHelper("addr", cur_addr->addr, Address);
-		  SetAddrStringHelper("netmask", cur_addr->netmask, Address);
-		  SetAddrStringHelper("broadaddr", cur_addr->broadaddr, Address);
-		  SetAddrStringHelper("dstaddr", cur_addr->dstaddr, Address);
-		  AddrArray->Set(Integer::New(j), Address);
-		}
+			if (cur_addr->addr){
+				int af = cur_addr->addr->sa_family;
+				if(af == AF_INET || af == AF_INET6){
+				  Local<Object> Address = Object::New();
+				  SetAddrStringHelper("addr", cur_addr->addr, Address);
+				  SetAddrStringHelper("netmask", cur_addr->netmask, Address);
+				  SetAddrStringHelper("broadaddr", cur_addr->broadaddr, Address);
+				  SetAddrStringHelper("dstaddr", cur_addr->dstaddr, Address);
+				  AddrArray->Set(Integer::New(j), Address);
+				}
             }
         }
 
@@ -105,7 +136,7 @@ FindAllDevs(const Arguments& args)
         if (cur_dev->flags & PCAP_IF_LOOPBACK) {
             Dev->Set(String::New("flags"), String::New("PCAP_IF_LOOPBACK"));
         }
-
+		
         DevsArray->Set(Integer::New(i), Dev);
     }
 
