31bffce396058e585453a1c4c28a3525d825ae36
jmaxxz <jmaxxz@users.noreply.github.com>
2015-02-15 15:17:15 -0600

    Skip encrypted packets

--- decode/radio_frame.js
+++ decode/radio_frame.js
@@ -11,7 +11,14 @@ RadioFrame.prototype.decode = function (raw_packet, offset) {
     ret.version = ret.frameControl & 0x0003;
     ret.type = (ret.frameControl >> 2) & 0x0003;
     ret.subType = (ret.frameControl >> 4) & 0x000f;
-    ret.flags = (ret.frameControl >> 8) & 0xff;
+    var flags = (ret.frameControl >> 8) & 0xff;
+    ret.flags = { raw:flags };
+    ret.flags.moreFragments = Boolean((flags >> 2) & 0x0001);
+    ret.flags.isRetry = Boolean((flags >> 3) & 0x0001);
+    ret.flags.moreData = Boolean((flags >> 5) & 0x0001);
+    ret.flags.encrypted = Boolean((flags >> 6) & 0x0001);
+    ret.flags.ordered = Boolean((flags >> 7) & 0x0001);
+
     ret.duration = raw_packet.readUInt16BE(offset, true); offset += 2;
     ret.bssid = new EthernetAddr(raw_packet, offset); offset += 6;
     ret.shost = new EthernetAddr(raw_packet, offset); offset += 6;
@@ -26,7 +33,9 @@ RadioFrame.prototype.decode = function (raw_packet, offset) {
     } else if (ret.type == 1) { //Control Frame
 
     } else if (ret.type == 2) { // Data Frame
-        if(ret.subType != 36) { // subType 36 is a null data frame
+        if (ret.flags.encrypted) {
+            //Just skip encrypted data for now.
+        } else if (ret.subType != 36) { // subType 36 is a null data frame
             ret.llc = new LogicalLinkControl().decode(raw_packet, offset);
         }
     }
