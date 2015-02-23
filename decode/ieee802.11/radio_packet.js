0bbe1b6311ea33cd0b04501f7919c30a3cfefe87
jmaxxz <jmaxxz@users.noreply.github.com>
2015-02-22 21:18:53 -0600

    Fix issue #143 handle different length radio tap headers

--- decode/ieee802.11/radio_packet.js
+++ decode/ieee802.11/radio_packet.js
@@ -1,52 +1,111 @@
 var RadioFrame = require("./radio_frame");
 
-function RadioPacket() {
+function PresentFieldFlags() {
+    this.tsft = undefined;
+    this.flags = undefined;
+    this.rate = undefined;
+    this.channel = undefined;
+    this.fhss = undefined;
+    this.signalStrength = undefined;
+    this.signalNoise = undefined;
+    this.lockQuality = undefined;
+    this.txAttenuation = undefined;
+    this.dbTxAttenuation = undefined;
+    this.dbmTxPower = undefined;
+    this.antenna = undefined;
+    this.dbAntennaSignal = undefined;
+    this.dbAntennaNoise = undefined;
+    this.rxFlags = undefined;
+}
 
+//flags should be a UInt32LE
+PresentFieldFlags.prototype.decode = function decode (flags) {
+    var r = flags;
+    this.tsft = Boolean(r & 0x0001) ;
+    this.flags = Boolean((r >> 1) & 0x0001);
+    this.rate = Boolean((r >> 2) & 0x0001);
+    this.channel = Boolean((r >> 3) & 0x0001);
+    this.fhss = Boolean((r >> 4) & 0x0001);
+    this.signalStrength = Boolean((r >> 5) & 0x0001);
+    this.signalNoise = Boolean((r >> 6) & 0x0001);
+    this.lockQuality = Boolean((r >> 7) & 0x0001);
+    this.txAttenuation = Boolean((r >> 8) & 0x0001);
+    this.dbTxAttenuation = Boolean((r >> 9) & 0x0001);
+    this.dbmTxPower = Boolean((r >> 10) & 0x0001);
+    this.antenna = Boolean((r >> 11) & 0x0001);
+    this.dbAntennaSignal = Boolean((r >> 12) & 0x0001);
+    this.dbAntennaNoise = Boolean((r >> 13) & 0x0001);
+    this.rxFlags = Boolean((r >> 14) & 0x0001);
+    return this;
+};
+
+function RadioPacket() {
+    this.headerRevision = undefined;
+    this.headerPad = undefined;
+    this.headerLength = undefined;
+    this.signalStrength = undefined;
+    this.frequency = undefined;
+    this.antenna = undefined;
+    this.ieee802_11Frame = undefined;
+    this.presentFields = undefined;
+    this.signalNoise = undefined;
 }
 
 RadioPacket.prototype.decode = function (raw_packet, offset) {
-    var ret = {};
     var original_offset = offset;
 
-    ret.headerRevision = raw_packet[offset++];
-    ret.headerPad = raw_packet[offset++];
-    ret.headerLength = raw_packet.readUInt16LE(offset); offset += 2;
+    this.headerRevision = raw_packet[offset++];
+    this.headerPad = raw_packet[offset++];
+    this.headerLength = raw_packet.readUInt16LE(offset); offset += 2;
 
-    //Preset Flags
+    //Present Flags
+    this.presentFields = new PresentFieldFlags().decode(
+        raw_packet.readUInt32LE(offset));
     offset += 4;
 
+    //alias presentFields as it will be used a lot
+    var p = this.presentFields;
+
     //MAC timestamp
-    offset += 8;
+    if(p.tsft) { offset += 8; }
 
     //Flags
-    offset += 2;
+    if(p.flags) { offset += 1; }
 
-    //Frequency
-    ret.frequency = raw_packet.readUInt16LE(offset); offset += 2;
+    if(p.rate) { offset += 1; }
 
-    //Channel type
-    offset += 2;
-
-    //SSI in DBI
-    ret.strength = -256 + raw_packet[offset++];
-
-    //Antenna
-    ret.antenna = raw_packet[offset++];
-
-    offset = original_offset + ret.headerLength;
-
-    ret.ieee802_11Frame = new RadioFrame().decode(raw_packet, offset);
-
-    if(ret.ieee802_11Frame && ret.ieee802_11Frame.llc && ret.ieee802_11Frame.llc.ip) {
-        ret.ip = ret.ieee802_11Frame.llc.ip;
-        delete ret.ieee802_11Frame.llc.ip;
-        ret.shost = ret.ieee802_11Frame.shost;
-        delete ret.ieee802_11Frame.shost;
-        ret.dhost = ret.ieee802_11Frame.dhost;
-        delete ret.ieee802_11Frame.dhost;
+    if(p.channel) {
+        //Frequency
+        this.frequency = raw_packet.readUInt16LE(offset, true);
+        //channel flags are the 2 bytes after channel freq
+        offset += 4;
     }
 
-    return ret;
+    if(p.fhss) { offset += 2; }
+
+    if(p.signalStrength) { //in dbi
+        this.signalStrength = raw_packet.readInt8(offset++);
+    }
+
+    if(p.signalNoise) { //in dbi
+        this.signalNoise = raw_packet.readInt8(offset++);
+    }
+    if(p.lockQuality) { offset += 2; }
+    if(p.txAttenuation) { offset++; }
+    if(p.dbTxAttenuation) { offset += 2; }
+    if(p.dbmTxPower) { offset++; }
+    if(p.antenna) { 
+        this.antenna = raw_packet[offset++];
+    }
+    if(p.dbAntennaSignal) { offset++; }
+    if(p.dbAntennaNoise) { offset++; }
+    if(p.rxFlags) { offset += 2; }
+
+    offset = original_offset + this.headerLength;
+
+    this.ieee802_11Frame = new RadioFrame().decode(raw_packet, offset);
+
+    return this;
 };
 
 module.exports = RadioPacket;
---
+++ spec/decode/ieee802.11/radio_packet.spec.js
@@ -0,0 +1,41 @@
+var RadioPacket = require("../../../decode/ieee802.11/radio_packet");
+
+describe("RadioPacket", function(){
+  var instance, probeExample1, probeExample2;
+  beforeEach(function () {
+
+    // a probe that has additional information in the header
+    probeExample2 = new Buffer("00001A002F480000000000000000000010026C09A000D8000000" + //Example of a radio tap header
+                              "40000000ffffffffffffe4ce8f16da48ffffffffffff804b" + // Probe request
+                              "0000010402040b16" + // i802.11 tags [ssid]
+                              "FFFF" , "hex"); // checksum, note this one is not valid
+
+    // a probe that has no additional information in the header
+    probeExample1 = new Buffer("000012002e48000000028509a000d3010000" + //Example of a radio tap header
+                              "40000000ffffffffffffe4ce8f16da48ffffffffffff804b" + // Probe request
+                              "0000010402040b16" + // i802.11 tags [ssid]
+                              "FFFF" , "hex"); // checksum, note this one is not valid
+    instance = new RadioPacket();
+  });
+
+  describe("#decode()", function(){
+    it("is a function", function(){
+        instance.decode.should.be.type("function");
+    });
+
+    it("sets #headerLength to be the length of the packet header", function() {
+      instance.decode(probeExample1, 0);
+      instance.should.have.property("headerLength", 18);
+    });
+
+    it("sets #signalStrength to be the signal strength in dbi", function(){
+      instance.decode(probeExample2, 0);
+      instance.should.have.property("signalStrength", -40);
+    });
+
+    it("handles different length packet headers", function() {
+      instance.decode(probeExample1, 0);
+      instance.should.have.property("signalStrength", -45);
+    });
+  });
+});
