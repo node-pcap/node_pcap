14b91fc8e16be0219568a4637cffa0924da54401
jmaxxz <jmaxxz@users.noreply.github.com>
2015-02-27 18:44:32 -0600

    Fix typos in ethernet_addr tests

--- spec/decode/ethernet_addr.spec.js
+++ spec/decode/ethernet_addr.spec.js
@@ -12,7 +12,7 @@ describe("EthernetAddr", function(){
         EthernetAddr.should.be.type("function");
     });
 
-    it("decodes ip address", function() {
+    it("decodes ethernet(MAC) address", function() {
       var instance = new EthernetAddr(exampleIp, 0);
       instance.should.have.property("addr", [1,2,3,4,5,6]);
     });
@@ -28,7 +28,7 @@ describe("EthernetAddr", function(){
       instance.toString.should.be.type("function");
     });
 
-    it("returns a string with octets delimited by :", function(){
+    it("returns a string like 01:02:03:04:05:06", function(){
       var result = instance.toString();
       result.should.be.exactly("01:02:03:04:05:06");
     });
