32b93f0b12798bb348cd3e2227898fd7fa4f3a8a
jmaxxz <jmaxxz@users.noreply.github.com>
2015-04-11 19:57:16 -0500

    Add tests for dns header

--- spec/decode/dns.spec.js
+++ spec/decode/dns.spec.js
@@ -21,6 +21,46 @@ describe("Dns", function(){
 
   describe("#decode()", function(){
     shouldBehaveLikeADecoder("dns", true);
+
+    it("sets #id to the transaction id", function() {
+      this.instance.decode(this.example, 0);
+      this.instance.should.have.property("id", 0x311f);
+    });
+
+    it("sets #header.isResponse to true if the packet was a response", function() {
+      this.instance.decode(this.example, 0);
+      this.instance.header.should.have.property("isResponse", false);
+    });
+
+    it("sets #header.opcode to the opcode", function() {
+      this.instance.decode(this.example, 0);
+      this.instance.header.should.have.property("opcode", 0);
+    });
+
+    it("sets #header.isAuthority to the true if this was sent by the authority for the domain", function() {
+      this.instance.decode(this.example, 0);
+      this.instance.header.should.have.property("isAuthority", false);
+    });
+
+    it("sets #header.isRecursionDesired to true if a recursive look up was desired", function() {
+      this.instance.decode(this.example, 0);
+      this.instance.header.should.have.property("isRecursionDesired", false);
+    });
+
+    it("sets #header.isRecursionAvailible to true if the server supports recursion", function() {
+      this.instance.decode(this.example, 0);
+      this.instance.header.should.have.property("isRecursionAvailible", false);
+    });
+
+    it("sets #header.z even to flags reserved for future use", function() {
+      this.instance.decode(this.example, 0);
+      this.instance.header.should.have.property("z", 0);
+    });
+
+    it("sets #header.responseCode even to the response code", function() {
+      this.instance.decode(this.example, 0);
+      this.instance.header.should.have.property("responseCode", 0);
+    });
   });
 
   describe("#toString()", function(){
