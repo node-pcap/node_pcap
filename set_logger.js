ca00da0079a1b9f5669e5bb6f7ac3f404e3ed487
Eric Caron <eric.caron@gmail.com>
2016-01-16 22:01:27 -0600

    Including code coverage for set_logger

--- set_logger.js
+++ set_logger.js
@@ -1,18 +1,24 @@
-function setLogger(logger) {
-    if (typeof logger === "undefined") {
-        this.logger = console;
-    } else if (logger !== false) {
-        this.logger = logger;
-    } else {
-        this.logger = {
-            log: function() { },
-            info: function() { },
-            warn: function() { },
-            error: function() { },
-            trace: function() { }
-        };
-    }
+var set_logger = require("../set_logger");
+require("should");
 
-}
+describe("set_logger", function(){
+    beforeEach(function () {
+        this.instance = {};
+        this.instance.logger = undefined;
+        this.instance.setLogger = set_logger;
+    });
 
-module.exports = setLogger;
+    it("attaches console by default", function(){
+        this.instance.setLogger();
+        this.instance.logger.should.be.an.instanceOf(Object).and.have.property("Console");
+    });
+    it("attaches a stubbed console on false", function(){
+        this.instance.setLogger(false);
+        this.instance.logger.should.be.an.instanceOf(Object).and.not.have.property("Console");
+    });
+    it("attaches whatever else is passed in, if defined and not false", function(){
+        // Demonstrates that we don't prevent the user from sending whatever they want to Logger
+        this.instance.setLogger("b");
+        this.instance.logger.should.equal("b");
+    });
+});
