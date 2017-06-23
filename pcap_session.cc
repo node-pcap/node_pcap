12534c24123f655dc3dab20a2be84d6ea101d055
Sumeet Patel <sumeetkpatel@gmail.com>
2017-06-23 15:51:58 +1000

    A snaplength option is now added and passed along to the libpcap calls. The node.js side api now has a appropriately sized buffer for incoming packets depending on the snaplength option.

--- pcap_session.cc
+++ pcap_session.cc
@@ -69,11 +69,15 @@ void PcapSession::PacketReady(u_char *s, const struct pcap_pkthdr* pkthdr, const
     }
 
     size_t copy_len = pkthdr->caplen;
-    if (copy_len > session->buffer_length) {
-        copy_len = session->buffer_length;
-    }
-    memcpy(session->buffer_data, packet, copy_len);
 
+    // if the length of the packet is actually bigger than the snaplength
+    // just use the snaplength, truncating the original.
+    if (copy_len > session->snap_length) {
+        copy_len = session->snap_length;
+    }
+
+    memcpy(session->buffer_data, packet, copy_len);
+    
     // copy header data to fixed offsets in second buffer from user
     memcpy(session->header_data, &(pkthdr->ts.tv_sec), 4);
     memcpy(session->header_data + 4, &(pkthdr->ts.tv_usec), 4);
@@ -129,7 +133,7 @@ void PcapSession::Open(bool live, const Nan::FunctionCallbackInfo<Value>& info)
     Nan::HandleScope scope;
     char errbuf[PCAP_ERRBUF_SIZE];
 
-    if (info.Length() == 6) {
+    if (info.Length() == 7) {
         if (!info[0]->IsString()) {
             Nan::ThrowTypeError("pcap Open: info[0] must be a String");
             return;
@@ -142,30 +146,35 @@ void PcapSession::Open(bool live, const Nan::FunctionCallbackInfo<Value>& info)
             Nan::ThrowTypeError("pcap Open: info[2] must be a Number");
             return;
         }
-        if (!info[3]->IsString()) {
-            Nan::ThrowTypeError("pcap Open: info[3] must be a String");
+        if (!info[3]->IsInt32()) {
+            Nan::ThrowTypeError("pcap Open: info[3] must be a Number");
             return;
         }
-        if (!info[4]->IsFunction()) {
-            Nan::ThrowTypeError("pcap Open: info[4] must be a Function");
+        if (!info[4]->IsString()) {
+            Nan::ThrowTypeError("pcap Open: info[4] must be a String");
             return;
         }
-        if (!info[5]->IsBoolean()) {
-            Nan::ThrowTypeError("pcap Open: info[5] must be a Boolean");
+        if (!info[5]->IsFunction()) {
+            Nan::ThrowTypeError("pcap Open: info[5] must be a Function");
+            return;
+        }
+        if (!info[6]->IsBoolean()) {
+            Nan::ThrowTypeError("pcap Open: info[6] must be a Boolean");
             return;
         }
     } else {
-        Nan::ThrowTypeError("pcap Open: expecting 6 arguments");
+        Nan::ThrowTypeError("pcap Open: expecting 7 arguments");
         return;
     }
     Nan::Utf8String device(info[0]->ToString());
     Nan::Utf8String filter(info[1]->ToString());
     int buffer_size = info[2]->Int32Value();
-    Nan::Utf8String pcap_output_filename(info[3]->ToString());
+    int snap_length = info[3]->Int32Value() || 65535;
+    Nan::Utf8String pcap_output_filename(info[4]->ToString());
 
     PcapSession* session = Nan::ObjectWrap::Unwrap<PcapSession>(info.This());
 
-    session->packet_ready_cb.Reset(info[4].As<Function>());
+    session->packet_ready_cb.Reset(info[5].As<Function>());
     session->pcap_dump_handle = NULL;
 
     if (live) {
@@ -182,7 +191,7 @@ void PcapSession::Open(bool live, const Nan::FunctionCallbackInfo<Value>& info)
         }
 
         // 64KB is the max IPv4 packet size
-        if (pcap_set_snaplen(session->pcap_handle, 65535) != 0) {
+        if (pcap_set_snaplen(session->pcap_handle, snap_length) != 0) {
             Nan::ThrowError("error setting snaplen");
             return;
         }
@@ -206,8 +215,8 @@ void PcapSession::Open(bool live, const Nan::FunctionCallbackInfo<Value>& info)
         }
 
         // fixes a previous to-do that was here.
-        if (info.Length() == 6) {
-            if (info[5]->Int32Value()) {
+        if (info.Length() == 7) {
+            if (info[6]->Int32Value()) {
                 if (pcap_set_rfmon(session->pcap_handle, 1) != 0) {
                     Nan::ThrowError(pcap_geterr(session->pcap_handle));
                     return;
